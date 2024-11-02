e0854e820448b758638c8b840ca8dc6b77eec797
z1pexR <alisaevramazan5@gmail.com>
2024-11-02 17:00:09 +0530

    add vc year

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('скуф')
+vg = int(input())
+a = False
+b = False
+
+if vg % 4 == 0:
+    a = True
+if vg % 100 != 0 or vg % 400 == 0:
+    b = True
+
+if b and a:
+    print('весокосный год')
+else:
+    print('обычный год')
