222aae79cd32d0c58e744f1686efcf685b6b0a08
z1pexR <alisaevramazan5@gmail.com>
2024-11-02 17:18:45 +0530

    new def

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 vg = int(input())
 a = False
-b = False
-
-if vg % 4 == 0:
-    a = True
-if vg % 100 != 0 or vg % 400 == 0:
-    b = True
-
-if b and a:
-    print('весокосный год')
-else:
-    print('обычный год')
+def vc(vg):
+    if vg % 100 != 0 or vg % 400 == 0 and vg % 4 == 0:
+        print('весокосный год')
+    else:
+        print('обычный год')
+    return vg
